0a87e3359df07a32941991e9f662af8d6c6c584d
wunderk1nd-e <elias@k1nd.ltd>
2019-03-26 14:28:14 +0000

    Fixing circleCI pipeline

--- build/tag.sh
+++ build/tag.sh
@@ -6,12 +6,34 @@ echo 'Fetching tag from remote...'
 git tag -l | xargs git tag -d
 git fetch --tags
 
-echo 'Extracting version from sbt project...'
-sbt release-write-next-version
-version=`cat target/next_release_version`
+# TODO Find a way to store the output
+if ! git describe --exact-match 2>/dev/null; then
+  echo 'Not tag found...'
 
-echo 'Tagging the current commit'
-git tag -a v${version} -m "Release version "${version}
+  last_tag=`git describe --abbrev=0 --tags`
+  current_version=${last_tag#'v'}
 
-echo 'Pushing tag v'${version}' to origin'
-git push origin v${version}
+  echo "Current version ${current_version}"
+
+  #replace . with space so can split into an array
+  current_version_parts=(${current_version//./ })
+
+  #get number parts and increase last one by 1
+  current_version_major=${current_version_parts[0]}
+  current_version_minor=${current_version_parts[1]}
+  current_version_build=${current_version_parts[2]}
+
+  next_version_build=$((current_version_build+1))
+  next_version="$current_version_major.$current_version_minor.$next_version_build"
+  next_tag="v${next_version}"
+
+  echo "Tagging the current commit with ${next_tag}"
+
+  git tag -a ${next_tag} -m "Release version "${next_version}
+
+  echo "Pushing tag ${next_tag} to origin"
+  git push origin ${next_tag}
+
+else
+  echo 'Tag found, no tag will be add'
+fi
